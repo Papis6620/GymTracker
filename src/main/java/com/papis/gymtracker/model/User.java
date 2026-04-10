43f2f50da26cc9a372b842ca5583d12a194b3afa
Patryk Krzywda <patryk.krzywda32@gmail.com>
2026-04-10 20:46:37 +0200

    Fix variable name

--- src/main/java/com/papis/gymtracker/model/User.java
+++ src/main/java/com/papis/gymtracker/model/User.java
@@ -29,7 +29,7 @@ public class User implements UserDetails {
     private String password;
 
     @Column(nullable = false)
-    private String username;
+    private String displayName;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
