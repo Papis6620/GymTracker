274029d77e780b1fcc8bdee1866bc09d7cb536cc
Patryk Krzywda <patryk.krzywda32@gmail.com>
2026-04-10 22:55:31 +0200

    Add service for register and authentication

---
+++ src/main/java/com/papis/gymtracker/service/AuthenticationService.java
@@ -0,0 +1,45 @@
+package com.papis.gymtracker.service;
+
+import com.papis.gymtracker.config.JwtService;
+import com.papis.gymtracker.dto.AuthenticationRequest;
+import com.papis.gymtracker.dto.AuthenticationResponse;
+import com.papis.gymtracker.dto.RegisterRequest;
+import com.papis.gymtracker.model.User;
+import com.papis.gymtracker.model.enums.Role;
+import com.papis.gymtracker.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request){
+        var user = User.builder()
+                .email(request.email())
+                .password(passwordEncoder.encode(request.password()))
+                .displayName(request.displayName())
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var token = jwtService.generateToken(user.getEmail());
+        return new AuthenticationResponse(token);
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.email(), request.password())
+        );
+        var user = userRepository.findByEmail(request.email())
+                .orElseThrow();
+        var token = jwtService.generateToken(user.getEmail());
+        return new AuthenticationResponse(token);
+    }
+}
