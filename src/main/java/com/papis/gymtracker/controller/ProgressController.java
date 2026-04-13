ceda59a3047fea1a03bac018a336d49a8b2ae7fe
Patryk Krzywda <patryk.krzywda32@gmail.com>
2026-04-13 19:27:13 +0200

    Add Progress service and controller

--- src/main/java/com/papis/gymtracker/controller/ProgressController.java
+++ src/main/java/com/papis/gymtracker/controller/ProgressController.java
@@ -1,4 +1,23 @@
 package com.papis.gymtracker.controller;
 
+import com.papis.gymtracker.dto.ProgressResponse;
+import com.papis.gymtracker.service.ProgressService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/progress")
+@RequiredArgsConstructor
 public class ProgressController {
+
+    private final ProgressService progressService;
+
+    @GetMapping("/{exerciseId}")
+    public List<ProgressResponse> getExerciseProgress(
+            @PathVariable String exerciseId
+    ){
+        return progressService.getExerciseProgress(exerciseId);
+    }
 }
--- src/main/java/com/papis/gymtracker/service/ProgressService.java
+++ src/main/java/com/papis/gymtracker/service/ProgressService.java
@@ -1,4 +1,30 @@
 package com.papis.gymtracker.service;
 
+import com.papis.gymtracker.dto.ProgressResponse;
+import com.papis.gymtracker.model.User;
+import com.papis.gymtracker.repository.UserRepository;
+import com.papis.gymtracker.repository.WorkoutEntryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ProgressService {
+    private final WorkoutEntryRepository workoutEntryRepository;
+    private final UserRepository userRepository;
+
+    public List<ProgressResponse> getExerciseProgress(String exerciseId){
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        return workoutEntryRepository.findByExerciseIdAndSessionUserIdOrderBySessionSessionDateAsc(exerciseId, user.getId())
+                .stream()
+                .map(ProgressResponse::from)
+                .toList();
+    }
+
 }
