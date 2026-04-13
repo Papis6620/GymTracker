01e3c1eb296cfe4daaf84c045abe2dd8f263c535
Patryk Krzywda <patryk.krzywda32@gmail.com>
2026-04-13 19:26:41 +0200

    Add Exercise service and controller

--- src/main/java/com/papis/gymtracker/controller/ExerciseController.java
+++ src/main/java/com/papis/gymtracker/controller/ExerciseController.java
@@ -1,4 +1,39 @@
 package com.papis.gymtracker.controller;
 
+import com.papis.gymtracker.dto.ExerciseResponse;
+import com.papis.gymtracker.model.enums.Category;
+import com.papis.gymtracker.model.enums.Equipment;
+import com.papis.gymtracker.model.enums.Level;
+import com.papis.gymtracker.service.ExerciseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/exercises")
+@RequiredArgsConstructor
 public class ExerciseController {
+    private final ExerciseService exerciseService;
+
+    @GetMapping
+    public List<ExerciseResponse> getAllExercises(
+            @RequestParam(required = false) String muscle,
+            @RequestParam(required = false) String level,
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) String equipment
+
+    ) {
+        if (muscle != null) return exerciseService.getExercisesByMuscleGroup(muscle);
+        if (level != null) return exerciseService.getExercisesByLevel(level);
+        if (category != null) return exerciseService.getExercisesByCategory(category);
+        if (equipment != null) return exerciseService.getExercisesByEquipment(equipment);
+        return exerciseService.getAllExercises();
+    }
+
+    @GetMapping("/{id}")
+    public ExerciseResponse getExerciseById(@PathVariable String id){
+        return exerciseService.getExerciseById(id);
+    }
+
 }
--- src/main/java/com/papis/gymtracker/service/ExerciseService.java
+++ src/main/java/com/papis/gymtracker/service/ExerciseService.java
@@ -1,4 +1,76 @@
 package com.papis.gymtracker.service;
 
+import com.papis.gymtracker.dto.ExerciseResponse;
+import com.papis.gymtracker.model.Exercise;
+import com.papis.gymtracker.model.enums.Category;
+import com.papis.gymtracker.model.enums.Equipment;
+import com.papis.gymtracker.model.enums.Level;
+import com.papis.gymtracker.repository.ExerciseRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ExerciseService {
+    private final ExerciseRepository exerciseRepository;
+
+    public List<ExerciseResponse> getAllExercises() {
+        return exerciseRepository.findAll().stream()
+                .map(ExerciseResponse::from)
+                .toList();
+    }
+
+    public ExerciseResponse getExerciseById(String id){
+        return exerciseRepository.findById(id)
+                .map(ExerciseResponse::from)
+                .orElseThrow(() -> new RuntimeException("Exercise not found"));
+    }
+
+    public List<ExerciseResponse> getExercisesByMuscleGroup(String muscleGroup){
+        return exerciseRepository.findByPrimaryMuscles_Name(muscleGroup.toLowerCase())
+                .stream()
+                .map(ExerciseResponse::from)
+                .toList();
+    }
+
+    public List<ExerciseResponse> getExercisesByLevel(String level){
+        try{
+            Level l = Level.valueOf(level.toUpperCase());
+            return exerciseRepository.findByLevel(l)
+                    .stream()
+                    .map(ExerciseResponse::from)
+                    .toList();
+        }catch(IllegalArgumentException e){
+            throw new RuntimeException("Invalid level provided: " + level);
+        }
+
+    }
+
+    public List<ExerciseResponse> getExercisesByEquipment(String equipment){
+        try{
+            Equipment e = Equipment.valueOf(equipment.toUpperCase());
+            return exerciseRepository.findByEquipment(e)
+                    .stream()
+                    .map(ExerciseResponse::from)
+                    .toList();
+        }catch (IllegalArgumentException e){
+        throw new RuntimeException("Invalid equipment provided: " + equipment);
+        }
+    }
+
+    public List<ExerciseResponse> getExercisesByCategory(String category){
+        try{
+            Category c = Category.valueOf(category.toUpperCase());
+            return exerciseRepository.findByCategory(c)
+                    .stream()
+                    .map(ExerciseResponse::from)
+                    .toList();
+        }catch (IllegalArgumentException e){
+            throw new RuntimeException("Invalid category provided: " + category);
+        }
+
+    }
 }
+
