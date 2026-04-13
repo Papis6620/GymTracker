439319d686569e63fc54076644a217b4bc3878e3
Patryk Krzywda <patryk.krzywda32@gmail.com>
2026-04-13 19:21:48 +0200

    Add Exercise DTO

--- src/main/java/com/papis/gymtracker/dto/ExerciseResponse.java
+++ src/main/java/com/papis/gymtracker/dto/ExerciseResponse.java
@@ -1,4 +1,38 @@
 package com.papis.gymtracker.dto;
 
-public record ExerciseResponse() {
+import com.papis.gymtracker.model.Exercise;
+import com.papis.gymtracker.model.MuscleGroup;
+import com.papis.gymtracker.model.enums.*;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public record ExerciseResponse(
+        String id,
+        String name,
+        Category category,
+        Level level,
+        Equipment equipment,
+        Force force,
+        Mechanic mechanic,
+        Set<String> primaryMuscles,
+        Set<String> secondaryMuscles
+) {
+    public static ExerciseResponse from(Exercise exercise){
+        return new ExerciseResponse(
+                exercise.getId(),
+                exercise.getName(),
+                exercise.getCategory(),
+                exercise.getLevel(),
+                exercise.getEquipment(),
+                exercise.getForce(),
+                exercise.getMechanic(),
+                exercise.getPrimaryMuscles().stream()
+                        .map(MuscleGroup::getName)
+                        .collect(Collectors.toSet()),
+                exercise.getSecondaryMuscles().stream()
+                        .map(MuscleGroup::getName)
+                        .collect(Collectors.toSet())
+        );
+    }
 }
