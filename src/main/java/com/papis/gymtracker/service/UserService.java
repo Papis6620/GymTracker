1cf9862f4f7f6eb712a06cd4351ded6ecb966335
Patryk Krzywda <patryk.krzywda32@gmail.com>
2026-04-20 11:43:55 +0200

    Add UserService
    Add UserService to eliminate code duplication

---
+++ src/main/java/com/papis/gymtracker/service/UserService.java
@@ -0,0 +1,19 @@
+package com.papis.gymtracker.service;
+
+import com.papis.gymtracker.model.User;
+import com.papis.gymtracker.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    public User getCurrentUser(){
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}
